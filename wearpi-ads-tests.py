0f8b7737d393860980fbec8e4b0ae459df8f3bda
Federico Visi <federico.visi@gmail.com>
2025-02-24 14:08:48 +0100

    raw values only

--- wearpi-ads-tests.py
+++ wearpi-ads-tests.py
@@ -21,8 +21,14 @@ chan2 = AnalogIn(ads, ADS.P2)
 chan3 = AnalogIn(ads, ADS.P3)
 
 
-print("{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format("raw0", "v0", "raw1", "v1", "raw2", "v2", "raw3", "v3",))
+# print("{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format("raw0", "v0", "raw1", "v1", "raw2", "v2", "raw3", "v3",))
+
+# while True:
+#     print("{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format(chan0.value, chan0.voltage, chan1.value, chan1.voltage, chan2.value, chan2.voltage, chan3.value, chan3.voltage))
+#     time.sleep(0.4)
+
+print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format("raw0", "raw1", "raw2", "raw3"))
 
 while True:
-    print("{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format(chan0.value, chan0.voltage, chan1.value, chan1.voltage, chan2.value, chan2.voltage, chan3.value, chan3.voltage))
+    print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format(chan0.value, chan1.value, chan2.value, chan3.value))
     time.sleep(0.4)
